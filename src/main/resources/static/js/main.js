2b9db5b4c4b62fe1f2ae4c4f5355782c33014154
tobi <tobias.fleckner@smartindustrycampus.onmicrosoft.com>
2025-05-09 13:40:22 +0200

    added an input and a function to get a product by id

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,4 @@
+//link this funktion to the button to call the GET request in the product controller
 function loadData() {
     fetch('/api/products')
         .then(response => response.text())
@@ -6,8 +7,27 @@ function loadData() {
         });
 }
 
+
+//link this function to the button if you want the output to say hello
 function sayHallo() {
     fetch('/hello')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('output').innerHTML = data;
+        });
+}
+
+//This function is used to get a product by its ID
+//If the input is empty, the output will say: "Please enter a product ID"
+function getProduct() {
+    const productID = document.getElementById('input').value;
+
+    if(!productID){
+        document.getElementById('output').innerHTML = "Please enter a product ID";
+        return;
+    }
+
+    fetch('/api/products/' + productID)
         .then(response => response.text())
         .then(data => {
             document.getElementById('output').innerHTML = data;
